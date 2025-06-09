2643ef95e1d22c777ac81d7dbd36fedbd1f4c486
dang.nguyen.viet <dang.nguyen.viet@pirago.work>
2025-06-09 17:24:22 +0700

    add keymap code-companion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -5,4 +5,30 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  keys = {
+    {
+      "<leader>ac",
+      "<cmd>CodeCompanionActions<cr>",
+      mode = { "n", "v" },
+      noremap = true,
+      silent = true,
+      desc = "CodeCompanion actions",
+    },
+    {
+      "<leader>aa",
+      "<cmd>CodeCompanionChat Toggle<cr>",
+      mode = { "n", "v" },
+      noremap = true,
+      silent = true,
+      desc = "CodeCompanion chat",
+    },
+    {
+      "<leader>ad",
+      "<cmd>CodeCompanionChat Add<cr>",
+      mode = "v",
+      noremap = true,
+      silent = true,
+      desc = "CodeCompanion add to chat",
+    },
+  },
 }
